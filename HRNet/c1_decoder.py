25acad58fd8f94eb28c4b400b50c25d22fd79f46
Ванна <vanna@MacBook-Pro-Daniil.local>
2020-05-13 00:16:12 +0300

    hrnet

---
+++ HRNet/c1_decoder.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch._utils
+from HRNet.batchnorm import SynchronizedBatchNorm2d
+
+import warnings
+warnings.filterwarnings("ignore")
+
+BatchNorm2d = SynchronizedBatchNorm2d
+
+
+def conv3x3_bn_relu(in_planes, out_planes, stride=1):
+    "3x3 convolution + BN + relu"
+    return nn.Sequential(
+            nn.Conv2d(in_planes, out_planes, kernel_size=3,
+                      stride=stride, padding=1, bias=False),
+            BatchNorm2d(out_planes),
+            nn.ReLU(inplace=True),
+            )
+
+class C1(nn.Module):
+    def __init__(self, num_class=150, fc_dim=2048, use_softmax=True):
+        super(C1, self).__init__()
+        self.use_softmax = use_softmax
+
+        self.cbr = conv3x3_bn_relu(fc_dim, fc_dim // 4, 1)
+
+        # last conv
+        self.conv_last = nn.Conv2d(fc_dim // 4, num_class, 1, 1, 0)
+
+    def forward(self, conv_out, segSize=None):
+        conv5 = conv_out[-1]
+        x = self.cbr(conv5)
+        x = self.conv_last(x)
+
+        if self.use_softmax:  # is True during inference
+            x = nn.functional.interpolate(
+                x, size=segSize, mode='bilinear', align_corners=False)
+            x = nn.functional.softmax(x, dim=1)
+        else:
+            x = nn.functional.log_softmax(x, dim=1)
+
+        return x
+
+    def init_weights(self, pretrained='', ):
+        #         logger.info('=> init weights from normal distribution')
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.normal_(m.weight, std=0.001)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+        if pretrained:
+            pretrained_dict = torch.load(pretrained, map_location=torch.device('cpu'))
+            #             logger.info('=> loading pretrained model {}'.format(pretrained))
+            model_dict = self.state_dict()
+            pretrained_dict = {k: v for k, v in pretrained_dict.items()
+                               if k in model_dict.keys()}
+            #             for k, _ in pretrained_dict.items():
+            #                 logger.info(
+            #                     '=> loading {} pretrained model {}'.format(k, pretrained))
+            model_dict.update(pretrained_dict)
+            self.load_state_dict(model_dict)
+
+
+def get_decoder(n_class, path):
+    model = C1(num_class=n_class, fc_dim=720)
+    model.init_weights(path)
+    return model
